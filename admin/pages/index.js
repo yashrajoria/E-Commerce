0e91c043bb1e4adc1c9e8526200c3aa26a4fe34d
yashrajoria <yashrajoria@gmail.com>
2023-08-02 18:17:46 +0100

    initial commit

---
+++ admin/pages/index.js
@@ -0,0 +1,27 @@
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
+
+export default function Home() {
+  const { data: session } = useSession();
+  console.log(session);
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+        Hello, <b>
+          {session?.user?.name}
+          </b>
+
+        </h2>
+        <div className="flex  bg-gray-300 text-black gap-1 rounded-lg">
+
+    <img src={session?.user?.image} alt="User Image" className="w-6 h-6 overflow-hidden"/>
+    <span className="px-2">
+
+    {session?.user?.name}
+    </span>
+        </div>
+      </div>
+    </Layout>
+  );
+}
