72882b53522cff741c0081223c8c1289bcade109
yashrajoria <yashrajoria@gmail.com>
2023-08-03 15:32:47 +0100

    Add additional properties for categories

--- admin/pages/categories.js
+++ admin/pages/categories.js
@@ -4,12 +4,12 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { withSwal } from "react-sweetalert2";
 
-
 function Category({ swal }) {
     const [name, setName] = useState("");
     const [editedCategory, setEditedCategory] = useState(null);
     const [categories, setCategories] = useState([]);
     const [parentCategory, setParentCategory] = useState("");
+    const [properties, setProperties] = useState([]);
     useEffect(() => {
         fetchCategories();
     }, []);
@@ -44,67 +44,96 @@ function Category({ swal }) {
 
     function editCategory(category) {
         try {
-            console.log(category._id)
+            console.log(category._id);
             setEditedCategory(category);
-            setName(category.name)
-            setParentCategory(category.parent?._id)
+            setName(category.name);
+            setParentCategory(category.parent?._id);
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
     }
 
     function deleteCategory(category) {
-        swal.fire({
-            title: "Are you sure?",
-            text: `Do you want to delete category ${category.name}?`,
-            showCancelButton: true,
-            cancelButtonText: 'Cancel',
-            confirmButtonText: 'Yes, Delete this category',
-            confirmButtonColor: "#d55",
-            reverseButtons: true
-        }).then(async result => {
-            if (result.isConfirmed) {
-                const catId = category._id
-                await axios.delete('/api/categories?_id=' + catId)
-                fetchCategories()
-            }
+        swal
+            .fire({
+                title: "Are you sure?",
+                text: `Do you want to delete category ${category.name}?`,
+                showCancelButton: true,
+                cancelButtonText: "Cancel",
+                confirmButtonText: "Yes, Delete this category",
+                confirmButtonColor: "#d55",
+                reverseButtons: true,
+            })
+            .then(async (result) => {
+                if (result.isConfirmed) {
+                    const catId = category._id;
+                    await axios.delete("/api/categories?_id=" + catId);
+                    fetchCategories();
+                }
+            });
+    }
+    function addProperty() {
+        setProperties(prev => {
+            return [...prev, { name: '', values: '' }]
         })
     }
-
-
     return (
         <Layout>
             <h1 className="text-blue-900 text-xl font-bold mb-2">Categories</h1>
             <label className="text-blue-900 text-l font-semi-bold mb-2">
-                {editedCategory ? `Edit Category ${editedCategory.name}` : "Create New Category"}
+                {editedCategory
+                    ? `Edit Category ${editedCategory.name}`
+                    : "Create New Category"}
             </label>
-            <form onSubmit={saveCategory} className="flex gap-1">
-                <input
-                    type="text"
-                    className="mb-0 w-full px-2 border-gray-300"
-                    placeholder={"Category Name"}
-                    onChange={(e) => setName(e.target.value)}
-                    value={name}
-                />
+            <form onSubmit={saveCategory}>
+                <div className="flex gap-1">
+                    <input
+                        type="text"
+                        className="mb-0 w-full px-2 border-gray-300"
+                        placeholder={"Category Name"}
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                    />
 
-                <select
-                    className="border-2 border-gray-300 rounded-md px-1 w-full"
-                    value={parentCategory || ""} // Set the value to an empty string if parentCategory is null
-                    onChange={(e) => setParentCategory(e.target.value || null)} // Set parentCategory to null if the user selects the option with an empty string value
-                >
-                    <option value="">No parent category</option>
-                    {categories.length > 0 && categories.map((category) => (
-                        <option key={category._id} value={category._id}>{category.name}</option>
+                    <select
+                        className=" border-2 border-gray-300 rounded-md px-1 w-full"
+                        value={parentCategory || ""} // Set the value to an empty string if parentCategory is null
+                        onChange={(e) => setParentCategory(e.target.value || null)} // Set parentCategory to null if the user selects the option with an empty string value
+                    >
+                        <option value="">No parent category</option>
+                        {categories.length > 0 &&
+                            categories.map((category) => (
+                                <option key={category._id} value={category._id}>
+                                    {category.name}
+                                </option>
+                            ))}
+                    </select>
+                </div>
+                <div className="mb-2">
+                    <label className="block">Properties</label>
+                    <button
+                        type="button"
+                        class="px-4 py-1 text-sm bg-blue-900 text-white font-semibold rounded-md border-blue-200"
+                        onClick={addProperty}
+                    >
+                        Add Property
+                    </button>
+
+                    {properties.length > 0 && properties.map(property => (
+                        <div className="gap-3 flex mt-3">
+                            <input className="border border-blue-700 px-1 py-2 rounded-md" type="text" placeholder="Property Name (eg: Color)" />
+                            <input type="text" className="border border-blue-700 rounded-md px-1 py-2" placeholder="Values, comma seperated" />
+                        </div>
                     ))}
-                </select>
-
+                </div>
                 <button
                     type="submit"
-                    className="bg-blue-900 text-white py-1 px-2 rounded-md"
+                    className="bg-blue-900 text-white py-1 px-2 rounded-md mt-2"
                 >
                     Save
                 </button>
             </form>
+
             <table className="w-full mt-4">
                 <thead className="bg-blue-100">
                     <tr className="border p-1 border-blue-200">
@@ -143,7 +172,10 @@ function Category({ swal }) {
                                         </svg>
                                         Edit
                                     </button>
-                                    <button className="bg-red-600 text-white px-2 py-1 rounded-md gap-1 inline-flex" onClick={() => deleteCategory(category)}>
+                                    <button
+                                        className="bg-red-600 text-white px-2 py-1 rounded-md gap-1 inline-flex"
+                                        onClick={() => deleteCategory(category)}
+                                    >
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             fill="none"
@@ -169,7 +201,4 @@ function Category({ swal }) {
     );
 }
 
-
-export default withSwal(({ swal }, ref) => (
-    <Category swal={swal} />
-))
+export default withSwal(({ swal }, ref) => <Category swal={swal} />);
