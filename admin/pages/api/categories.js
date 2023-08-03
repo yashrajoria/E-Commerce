7fc8b9cdc641723fcc23cc5b173066ef7fd44343
yashrajoria <yashrajoria@gmail.com>
2023-08-03 13:33:11 +0100

    Edit and delete category

--- admin/pages/api/categories.js
+++ admin/pages/api/categories.js
@@ -17,4 +17,16 @@ export default async function newCategory(req, res) {
         const cateDoc = await Category.create({ name, parent: parentCategory })
         res.json(cateDoc)
     }
+    if (method === "PUT") {
+        const { name, parentCategory, _id } = req.body
+        console.log(name)
+        const cateDoc = await Category.updateOne({ _id }, { name, parent: parentCategory })
+        res.json(cateDoc)
+    }
+
+    if (method === "DELETE") {
+        const { _id } = req.query
+        await Category.deleteOne({ _id })
+        res.json("Category deleted")
+    }
 }
--- admin/pages/categories.js
+++ admin/pages/categories.js
@@ -2,13 +2,16 @@ import Layout from "@/components/Layout";
 import { data } from "autoprefixer";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { withSwal } from "react-sweetalert2";
 
-export default function Categories() {
+
+function Category({ swal }) {
     const [name, setName] = useState("");
+    const [editedCategory, setEditedCategory] = useState(null);
     const [categories, setCategories] = useState([]);
-    const [parentCategory, setParentCategory] = useState("")
+    const [parentCategory, setParentCategory] = useState("");
     useEffect(() => {
-        fetchCategories()
+        fetchCategories();
     }, []);
     function fetchCategories() {
         axios.get("/api/categories").then((result) => {
@@ -17,16 +20,49 @@ export default function Categories() {
     }
     async function saveCategory(e) {
         e.preventDefault();
-        await axios.post("/api/categories", { name, parentCategory });
+        const data = { name, parentCategory }
+        if (editedCategory) {
+            data._id = editedCategory._id
+            await axios.put("/api/categories", data);
+            setEditedCategory(null)
+        } else {
+            await axios.post("/api/categories", data);
+        }
         setName("");
         fetchCategories()
     }
+
+    function editCategory(category) {
+        console.log(category)
+        setEditedCategory(category);
+        setName(category.name)
+        setParentCategory(category.parent?._id)
+    }
+
+    function deleteCategory(category) {
+        swal.fire({
+            title: "Are you sure?",
+            text: `Do you want to delete category ${category.name}?`,
+            showCancelButton: true,
+            cancelButtonText: 'Cancel',
+            confirmButtonText: 'Yes, Delete this category',
+            confirmButtonColor: "#d55",
+            reverseButtons: true
+        }).then(async result => {
+            if (result.isConfirmed) {
+                const catId = category._id
+                await axios.delete('/api/categories?_id=' + catId)
+                fetchCategories()
+            }
+        })
+    }
+
 
     return (
         <Layout>
             <h1 className="text-blue-900 text-xl font-bold mb-2">Categories</h1>
             <label className="text-blue-900 text-l font-semi-bold mb-2">
-                New Category Name
+                {editedCategory ? `Edit Category ${editedCategory.name}` : "Create New Category"}
             </label>
             <form onSubmit={saveCategory} className="flex gap-1">
                 <input
@@ -36,12 +72,16 @@ export default function Categories() {
                     onChange={(e) => setName(e.target.value)}
                     value={name}
                 />
-                <select className="border-2 border-gray-300 rounded-md px-1 w-full " value={parentCategory}
-                    onChange={e => setParentCategory(e.target.value)}>
+
+                <select
+                    className="border-2 border-gray-300 rounded-md px-1 w-full "
+                    value={parentCategory}
+                    onChange={(e) => setParentCategory(e.target.value)}
+                >
                     <option value="0">No Parent Category</option>
                     {categories.length > 0 &&
                         categories.map((category) => (
-                            <option value={category._id} >{category.name}</option>
+                            <option value={category._id}>{category.name}</option>
                         ))}
                 </select>
                 <button
@@ -56,6 +96,7 @@ export default function Categories() {
                     <tr className="border p-1 border-blue-200">
                         <td className="border p-1 border-blue-200">Category Name</td>
                         <td className="border p-1 border-blue-200">Parent Category</td>
+                        <td className="border p-1 border-blue-200"></td>
                     </tr>
                 </thead>
                 <tbody>
@@ -64,7 +105,48 @@ export default function Categories() {
                             <tr className="border p-1 border-blue-200">
                                 <td className="border p-1 border-blue-200">{category.name}</td>
                                 {/* important */}
-                                <td className="border p-1 border-blue-200">{category?.parent?.name}</td>
+                                <td className="border p-1 border-blue-200">
+                                    {category?.parent?.name}
+                                </td>
+                                <td className="border p-1 border-blue-200 flex items-center gap-2">
+                                    <button
+                                        className="bg-blue-900 text-white px-2 py-1 rounded-md gap-1 inline-flex"
+                                        onClick={() => editCategory(category)}
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            strokeWidth="1.5"
+                                            stroke="currentColor"
+                                            className="w-4 h-4"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                                            />
+                                        </svg>
+                                        Edit
+                                    </button>
+                                    <button className="bg-red-600 text-white px-2 py-1 rounded-md gap-1 inline-flex" onClick={() => deleteCategory(category)}>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            strokeWidth={1.5}
+                                            stroke="currentColor"
+                                            className="w-6 h-6"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                            />
+                                        </svg>
+                                        Delete
+                                    </button>
+                                </td>
                             </tr>
                         ))}
                 </tbody>
@@ -72,3 +154,8 @@ export default function Categories() {
         </Layout>
     );
 }
+
+
+export default withSwal(({ swal }, ref) => (
+    <Category swal={swal} />
+))
