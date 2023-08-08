146aeeaf842aaea9d98181493a689514e6d7a3b1
yashrajoria <yashrajoria@gmail.com>
2023-08-08 19:17:28 +0100

    Admin panel fetches order

---
+++ admin/pages/orders.js
@@ -0,0 +1,50 @@
+import Layout from "@/components/Layout";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+function Orders() {
+  const [orders, setOrders] = useState();
+  useEffect(() => {
+    axios.get("/api/orders").then((response) => {
+      setOrders(response.data);
+    });
+  }, []);
+  return (
+    <Layout>
+      <h1>Orders</h1>
+      <table className="w-full mt-4">
+        <thead className="bg-blue-100">
+          <tr className="border p-1 border-blue-200">
+            <th className="border p-1 border-blue-200">Date</th>
+            <th className="border p-1 border-blue-200">Recipient</th>
+            <th className="border p-1 border-blue-200">Products</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders?.length > 0 &&
+            orders.map((order) => (
+              <tr>
+                <td>{order.createdAt}</td>
+                <td>
+                  {order.name} {order.email} <br />
+                  {order.city} {order.pCode} {order.country} <br />
+                  {order.address} {order.phone}
+                  <br />
+                </td>
+                <td>
+                  {order.line_items.map((l) => (
+                    <>
+                      {l.price_data?.product_data.name} x {l.quantity}
+                      <br />
+                    </>
+                  ))}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+}
+
+export default Orders;
