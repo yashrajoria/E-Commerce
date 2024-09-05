c32d0fe615efb71b33d0d9d8a25ed64d62311543
yash.rajoria1 <yash.rajoria@zorang.com>
2024-09-05 20:31:54 +0100

    Redux implementaion

--- admin/pages/_app.js
+++ admin/pages/_app.js
@@ -1,12 +1,16 @@
+import { store } from "@/redux/store";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { Provider } from "react-redux";
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <Provider store={store}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </Provider>
   );
 }
