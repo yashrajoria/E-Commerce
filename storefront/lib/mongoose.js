6c09fcaca5d415e6304e55eb0f4e6474e83ff5d8
Yash Rajoria <yashrajoria@gmail.com>
2025-05-01 11:25:26 +0530

    fix: Enhance mongooseConnect function with error handling and connection logging

--- storefront/lib/mongoose.js
+++ storefront/lib/mongoose.js
@@ -5,6 +5,16 @@ export function mongooseConnect() {
     return mongoose.connection.asPromise();
   } else {
     const uri = process.env.MONGODB_URI;
-    return mongoose.connect(uri);
+    if (!uri) {
+      throw new Error("MONGODB_URI is not defined in environment variables.");
+    }
+
+    console.log("Connecting to MongoDB...");
+    return mongoose
+      .connect(uri, {
+        dbName: "ecommerce",
+      })
+      .then(() => console.log("MongoDB connected"))
+      .catch((err) => console.error("MongoDB connection error:", err));
   }
 }
