1d0aacd32b0d523c1c71bab695b4aec590f74dba
Yash <yashrajoria@gmail.com>
2025-07-17 23:42:42 +0530

    refactor: Clean up order API handler by removing commented code and improving readability

--- admin-new/pages/api/orders/index.ts
+++ admin-new/pages/api/orders/index.ts
@@ -5,6 +5,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // console.log(req.method);
   if (req.method === "GET") {
     const query = req.query;
 
@@ -12,10 +13,27 @@ export default async function handler(
 
     const pageNumber = parseInt(page as string, 10) || 1;
     const limitNumber = parseInt(limit as string, 10) || 10;
-    const offset = (pageNumber - 1) * limitNumber;
-
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_NEW_API_URL}orders`);
-
-    console.log(response);
+    // const offset = (pageNumber - 1) * limitNumber;
+    const cookie = req.headers.cookie;
+    const apiRes = await axios.get(
+      `${process.env.NEXT_PUBLIC_NEW_API_URL}orders`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: cookie,
+        },
+        params: {
+          page: pageNumber,
+          limit: limitNumber,
+        },
+        withCredentials: true,
+      }
+    );
+    // console.log({ apiRes });
+    const orders = apiRes?.data?.orders;
+    const meta = apiRes?.data?.meta;
+    // console.log({ orders });
+    // console.log({ meta });
+    res.status(200).json({ orders, meta });
   }
 }
