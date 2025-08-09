cabd90b436ba8b72b42effdb8aea3bd700e38755
Yash <yashrajoria@gmail.com>
2025-08-09 17:35:03 +0530

    feat(api): Auth API

---
+++ storefront-new/storefront/src/pages/api/auth.ts
@@ -0,0 +1,33 @@
+import { axiosInstance } from "@/utils/axiosInstance";
+import { API_ROUTES } from "./constants/apiRoutes";
+
+export const loginUser = async (email: string, password: string) => {
+  console.log(email, password);
+  const response = await axiosInstance.post(API_ROUTES.AUTH.LOGIN, {
+    email,
+    password,
+    role: "user",
+  });
+  return response.data;
+};
+export const registerUser = async (
+  email: string,
+  password: string,
+  fullName: string
+) => {
+  const response = await axiosInstance.post(API_ROUTES.AUTH.REGISTER, {
+    email,
+    password,
+    name: fullName,
+    role: "user",
+  });
+  return response.data;
+};
+
+export const verifyEmail = async (email: string, code: string) => {
+  const response = await axiosInstance.post(API_ROUTES.AUTH.VERIFY_EMAIL, {
+    email,
+    code,
+  });
+  return response.data;
+};
