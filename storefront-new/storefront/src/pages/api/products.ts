17dcffc97292cce333ab5f271fa903f47a2e9754
Yash <yashrajoria@gmail.com>
2025-08-09 17:35:19 +0530

    feat(api): Products  API

---
+++ storefront-new/storefront/src/pages/api/products.ts
@@ -0,0 +1,7 @@
+import { axiosInstance } from "@/utils/axiosInstance";
+import { API_ROUTES } from "./constants/apiRoutes";
+
+export const fetchAllProducts = async () => {
+  const response = await axiosInstance.get(API_ROUTES.PRODUCTS.ALL);
+  return response.data;
+};
