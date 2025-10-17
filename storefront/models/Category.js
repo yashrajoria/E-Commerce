25675749e9858fc30e33561a5f9ed9acf225e4cd
Yash <yashrajoria@gmail.com>
2025-10-17 16:22:02 +0100

    removed deprecated files

--- storefront/models/Category.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema, model, models } from "mongoose";
-
-const CategorySchema = new Schema({
-  name: { type: String, required: true },
-  parent: { type: Schema.Types.ObjectId, ref: "Category" },
-  properties: [{ type: Schema.Types.Mixed }],
-});
-
-export const Category = models.Category || model("Category", CategorySchema);
