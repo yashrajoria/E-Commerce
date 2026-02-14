f86ea87d4cd8069c4cf175068f4a3b06363ebc20
Yash <yashrajoria@gmail.com>
2026-02-14 15:07:22 +0000

    Refactor API routes to use API_BASE_URL for consistency and maintainability

--- storefront-new/storefront/src/pages/api/constants/apiRoutes.ts
+++ storefront-new/storefront/src/pages/api/constants/apiRoutes.ts
@@ -1,40 +1,42 @@
+const API_BASE_URL = "http://localhost:8080";
 export const API_ROUTES = {
   AUTH: {
-    LOGIN: `/auth/login`,
-    REGISTER: `/auth/register`,
-    VERIFY_EMAIL: `/auth/verify-email`,
-    RESEND_VERIFICATION: `/auth/resend-verification`,
-    REFRESH: `/auth/refresh`,
-    REQUEST_PASSWORD_RESET: `/auth/request-password-reset`,
-    RESET_PASSWORD: `/auth/reset-password`,
-    LOGOUT: `/auth/logout`,
-    STATUS: `/auth/status`,
+    LOGIN: `${API_BASE_URL}/auth/login`,
+    REGISTER: `${API_BASE_URL}/auth/register`,
+    VERIFY_EMAIL: `${API_BASE_URL}/auth/verify-email`,
+    RESEND_VERIFICATION: `${API_BASE_URL}/auth/resend-verification`,
+    REFRESH: `${API_BASE_URL}/auth/refresh`,
+    REQUEST_PASSWORD_RESET: `${API_BASE_URL}/auth/request-password-reset`,
+    RESET_PASSWORD: `${API_BASE_URL}/auth/reset-password`,
+    LOGOUT: `${API_BASE_URL}/auth/logout`,
+    STATUS: `${API_BASE_URL}/auth/status`,
   },
   USER: {
-    PROFILE: "users/profile/",
-    UPDATE_PASSWORD: "users/change-password",
-    UPDATE_USER_DATA: "users/profile",
+    PROFILE: `${API_BASE_URL}/users/profile`,
+    UPDATE_PASSWORD: `${API_BASE_URL}/users/change-password`,
+    UPDATE_USER_DATA: `${API_BASE_URL}/users/profile`,
   },
 
   PRODUCTS: {
-    ALL: "products",
-    BY_ID: (id: string) => `products/${id}`,
+    ALL: `${API_BASE_URL}/products`,
+    BY_ID: (id: string) => `${API_BASE_URL}/products/${id}`,
   },
 
   CATEGORIES: {
-    ALL: `/categories/`,
+    ALL: `${API_BASE_URL}/categories/`,
   },
 
   ORDERS: {
-    ALL: "orders",
-    BY_ID: (id: string) => `orders/${id}`,
+    ALL: `${API_BASE_URL}/orders`,
+    BY_ID: (id: string) => `${API_BASE_URL}/orders/${id}`,
   },
   CART: {
-    ADD: "cart/add",
-    CHECKOUT: "cart/checkout",
+    ADD: `${API_BASE_URL}/cart/add`,
+    CHECKOUT: `${API_BASE_URL}/cart/checkout`,
   },
   PAYMENT: {
-    STATUS_BY_ORDER: (id: string) => `payment/status/by-order/${id}`,
-    VERIFY: "payment/verify-payment",
+    STATUS_BY_ORDER: (id: string) =>
+      `${API_BASE_URL}/payment/status/by-order/${id}`,
+    VERIFY: `${API_BASE_URL}/payment/verify-payment`,
   },
 };
