1c98676347eb6f2f42bc95e906f68f86a892cabe
Yash <yashrajoria@gmail.com>
2026-03-20 22:59:50 +0000

    feat: add storefront theme classes to HTML and body elements in document

--- admin-new/pages/_document.tsx
+++ admin-new/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="theme-storefront">
       <Head />
-      <body className="antialiased">
+      <body className="antialiased theme-storefront">
         <Main />
         <NextScript />
       </body>
