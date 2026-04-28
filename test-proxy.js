dd982d7dc5a10c79f541a4ff6f55da763572d90a
Yash <yashrajoria@gmail.com>
2026-04-28 16:07:56 +0100

    refactor: remove obsolete test proxy and request files

--- test-proxy.js
+++
@@ -1,2 +0,0 @@
-const { proxyRequest, getBackendBaseUrl } = require('./packages/shared/dist/utils/apiProxy.js') || {};
-console.log('Backend URL:', getBackendBaseUrl ? getBackendBaseUrl() : 'Not exported');
